3431f00087914714fc341302a2112642f7bc3a5d
Abubakar Shaikh <shaikhabubakar2380@gmail.com>
2023-08-21 09:46:53 +0530

    Rapid api key secured using env

--- helpers/ytApi.js
+++ helpers/ytApi.js
@@ -4,7 +4,7 @@ export async function ytApi(ytId) {
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6fabfe3ba0msha10853256d5c5f9p1c1247jsnf1625ea46cb6',
+		'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
 		'X-RapidAPI-Host': 'ytstream-download-youtube-videos.p.rapidapi.com'
 	}
 };
